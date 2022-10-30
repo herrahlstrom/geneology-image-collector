2c7aa44a35690f85617ec23d6621e518645335f4
Martin Ahlström <martin.ahlstrom@outlook.com>
2022-10-30 16:23:32 +0100

    Add MyGenDbContext to service injector and create an importer of imageas and connection to persons

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyGen.Data;
 using System.IO;
 using System.Windows;
 
@@ -34,12 +35,21 @@ namespace GeneologyImageCollector
                         optionsBuilder.UseSqlite(connString, contextOptionsBuilder => contextOptionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
                     });
 
+                    services.AddDbContextFactory<MyGenDbContext>((services, optionsBuilder) =>
+                    {
+                        var config = services.GetRequiredService<IConfiguration>();
+                        var connString = config.GetConnectionString("MyGen");
+                        optionsBuilder.UseSqlite(connString, contextOptionsBuilder => contextOptionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
+                    });
+
                     services.AddSingleton<AppSettings>();
 
                     // View models
                     services
                         .AddTransient<IMainViewModel, MainViewModel>();
 
+                    // Helpers
+                    services.AddTransient<MyGenImporter>();
                 }).Build();
         }
 
@@ -58,10 +68,28 @@ namespace GeneologyImageCollector
         {
             await AppHost.StartAsync();
 
+            await InitDatabase();
+
             MainWindow = new MainWindow();
             MainWindow.Show();
 
             base.OnStartup(e);
+        }
+
+        private async Task InitDatabase()
+        {
+            var db = AppHost.Services.GetRequiredService<AppDbContext>();
+
+            //if (Debugger.IsAttached)
+            //{
+            //    await db.Database.EnsureDeletedAsync();
+            //}
+
+            if (await db.Database.EnsureCreatedAsync())
+            {
+                var importer = AppHost.Services.GetRequiredService<MyGenImporter>();
+                await importer.Import();
+            }
         }
     }
 }
---
+++ src/MyGenImporter.cs
@@ -0,0 +1,150 @@
+﻿using GeneologyImageCollector.Data;
+using GeneologyImageCollector.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using MyGen.Data;
+using System.IO;
+
+namespace GeneologyImageCollector;
+
+internal class MyGenImporter
+{
+    private readonly AppSettings _settings;
+    private AppDbContext _db;
+    private MyGenDbContext _myGen;
+    private Dictionary<int, Guid> _persons = new();
+
+    private const int FamilyHusbandMember = 1;
+    private const int FamilyWifeMember = 2;
+
+    public MyGenImporter(AppDbContext db, MyGenDbContext myGen, AppSettings settings)
+    {
+        _db = db;
+        _myGen = myGen;
+        _settings = settings;
+    }
+
+    public async Task Import()
+    {
+        await ImportTypes();
+        await ImportPortraits();
+        await ImportSourceImages();
+
+        await _db.SaveChangesAsync();
+    }
+
+    private void AddPersonsToImage(Guid imageId, IEnumerable<int> personIds)
+    {
+        foreach (var pId in personIds)
+        {
+            if (!_persons.TryGetValue(pId, out Guid personId))
+            {
+                var name = _myGen.Persons
+                    .Where(x => x.Id == pId)
+                    .Select(x => x.Firstname + " " + x.Lastname)
+                    .First().Replace("*", "");
+                var personEntity = new Person { Id = Guid.NewGuid(), Name = name };
+                _db.Persons.Add(personEntity);
+
+                personId = personEntity.Id;
+                _persons.Add(pId, personId);
+            }
+
+            _db.PersonImages.Add(new PersonImage { ImageId = imageId, PersonId = personId });
+        }
+    }
+
+    private async Task ImportPortraits()
+    {
+        var media = await _myGen.Media.Where(m => m.Path != null && m.Path != "").ToListAsync();
+        var typeId = _db.ImageTypes.Where(x => x.Key == "portrait").Select(x => x.Id).Single();
+
+        foreach (var m in media)
+        {
+            bool exists = File.Exists(Path.Combine(_settings.RootPath, "Foton", m.Path));
+
+            Image imgEntity = new Image
+            {
+                Id = Guid.NewGuid(),
+                TypeId = typeId,
+                Title = m.Path,
+                Notes = "",
+                Added = DateTime.UtcNow,
+                Missing = !exists,
+                Path = $"Foton\\{m.Path}",
+            };
+
+            _db.Images.Add(imgEntity);
+
+            List<int> personIds = await _myGen.MediaPersons.Where(x => x.MediaId == m.Id).Select(x => x.PersonId).ToListAsync();
+            AddPersonsToImage(imgEntity.Id, personIds);
+        }
+    }
+
+    private async Task ImportSourceImages()
+    {
+        var sources = await _myGen.Sources.Where(m => m.ImagePath != null && m.ImagePath != "").ToListAsync();
+        var typeId = _db.ImageTypes.Where(x => x.Key == "source").Select(x => x.Id).Single();
+
+        foreach (var s in sources)
+        {
+            bool exists = File.Exists(Path.Combine(_settings.RootPath, "Källor", s.ImagePath!));
+
+            Image imgEntity = new Image
+            {
+                Id = Guid.NewGuid(),
+                TypeId = typeId,
+                Title = s.Name,
+                Notes = s.Notes,
+                Added = DateTime.UtcNow,
+                Missing = !exists,
+                Path = $"Källor\\{s.ImagePath}",
+            };
+
+            _db.Images.Add(imgEntity);
+
+            var lifeStoryIds = await _myGen.LifeStorySources
+                .Where(x => x.SourceId == s.Id)
+                .Select(x => x.LifeStoryId).ToListAsync();
+
+            List<int> personIds = await _myGen.LifeStoryMembers.Where(x => lifeStoryIds.Contains(x.LifeStoryId)).Select(x => x.PersonId).ToListAsync();
+
+            var familyIds = await _myGen.FamilyLifeStories.Where(x => lifeStoryIds.Contains(x.LifeStoryId)).Select(x => x.FamilyId).ToListAsync();
+            if (familyIds.Count > 0)
+            {
+                personIds.AddRange(await _myGen.FamilyMembers
+                    .Where(x => familyIds.Contains(x.FamilyId))
+                    .Where(x => x.MemberTypeId == FamilyHusbandMember || x.MemberTypeId == FamilyWifeMember)
+                    .Select(x => x.PersonId)
+                    .ToListAsync());
+            }
+
+            AddPersonsToImage(imgEntity.Id, personIds.Distinct());
+        }
+    }
+
+    private async Task ImportTypes()
+    {
+        _db.ImageTypes.Add(new ImageType
+        {
+            Id = Guid.NewGuid(),
+            Key = "",
+            Name = "Övrig"
+        });
+
+        _db.ImageTypes.Add(new ImageType
+        {
+            Id = Guid.NewGuid(),
+            Key = "portrait",
+            Name = "Porträtt"
+        });
+
+        _db.ImageTypes.Add(new ImageType
+        {
+            Id = Guid.NewGuid(),
+            Key = "source",
+            Name = "Källa"
+        });
+
+        await _db.SaveChangesAsync();
+    }
+}
